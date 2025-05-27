671eb5895f90651b104da5475886886c8c91ab9d
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-05-27 16:15:26 +0500

    Refactor logger to use PKT timestamp format for console and file outputs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,21 +2,44 @@ import { createLogger, format, transports } from "winston";
 
 const { combine, timestamp, printf, colorize, json, uncolorize } = format;
 
-// 1) Console: colored, timestamped
+// 1)  PKT timestamp function
+const pakistanTime = () =>
+  new Intl.DateTimeFormat("en-PK", {
+    timeZone: "Asia/Karachi",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  })
+    .format(new Date())
+    .replace(/(\d{2})\/(\d{2})\/(\d{4}), (\d{2}:\d{2}:\d{2})/, "$3-$2-$1 $4");
+
+// 2) Console: colored, timestamped
 const consoleFormat = combine(
   colorize(),
+  timestamp({ format: pakistanTime }),
   printf(({ level, message, timestamp }) => {
-    return `${level}: ${message}`;
+    return ` ${level}: ${message} at ${timestamp}`;
   })
 );
 
-// 2) File: uncolored JSON with ISO timestamp
-const fileFormat = combine(uncolorize(), timestamp(), json());
+// 3) File: uncolored JSON, PKT timestamp
+const fileFormat = combine(
+  uncolorize(),
+  timestamp({ format: pakistanTime }),
+  json()
+);
 
 const logger = createLogger({
   level: "info",
   transports: [
+    // Console transport:
     new transports.Console({ format: consoleFormat }),
+
+    // File transport:
     new transports.File({
       filename: "app.log",
       format: fileFormat,
