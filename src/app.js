880078309a072b67a350c68c056c183301be199d
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-05-27 15:52:08 +0500

    Fix middleware comments and import logger for improved logging

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
+import logger from "./utils/logger.js";
 
 const app = express();
 
@@ -22,9 +23,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// morgan format for logging
 const morganFormat = ":method :url :status :response-time ms";
 
-// middlewear
+// middleware
 app.use(
   morgan(morganFormat, {
     stream: {
@@ -40,5 +42,12 @@ app.use(
     },
   })
 );
+
+//routes
+//import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+//routes
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
