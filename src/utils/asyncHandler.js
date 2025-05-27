4d2e00b74b7e24dfc4226eaf309f92047d4fb90a
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-05-27 15:51:45 +0500

    Fix typo in asyncHandler function name and export statement

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
-const asycnHandler = (requestHandler) => {
+const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
 
-export { asycnHandler };
+export { asyncHandler };
