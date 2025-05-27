b37ffea1926076b549ac31ce7a3a1d97153b1420
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-05-27 17:33:29 +0500

    Add user model with password hashing and token generation methods

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import brycrpt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const userSchema = new Schema(
   {
     username: {
@@ -46,4 +49,34 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = brycrpt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await brycrpt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+userSchema.methods.generateRefereshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 export const User = mongoose.model("User", userSchema);
