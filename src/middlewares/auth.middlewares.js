63543b2421303d6c4917c996b4ee73f8f456b440
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-05-29 00:52:28 +0500

    Add JWT verification middleware for user authentication

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,28 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import jwt from "jsonwebtoken";
+
+const verifyJWT = asyncHandler(async (req, _, next) => {
+  const token =
+    req.cookies.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    throw new ApiError(401, "Unauthorized");
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decoded?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      throw new ApiError(401, "User not found");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
+
+export { verifyJWT };
