8f6321273494d88fde4857b0ef9bbe2b48e3b051
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-05-29 14:11:45 +0500

    Add user account management routes: change password, get current user, update account details, and upload avatar/cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,11 @@ import {
   registerUser,
   logOutUser,
   loginUser,
+  getCurrentUser,
+  updateAccountDetails,
+  changeCurrentPassword,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -20,4 +25,13 @@ router.route("/register").post(
 // secured routes
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/login").post(loginUser);
+router.route("/change-password").patch(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/update-avatar")
+  .patch(upload.single("avatar"), verifyJWT, updateUserAvatar);
+router
+  .route("/update-cover-image")
+  .patch(upload.single("coverImage"), verifyJWT, updateUserCoverImage);
 export default router;
