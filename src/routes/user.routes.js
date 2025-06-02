d15015b531956ac9c9a1c6e1c26af3f591d73246
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-06-02 18:55:48 +0500

    Add user channel profile and watch history routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,6 +10,8 @@ import {
   updateUserAvatar,
   updateUserCoverImage,
   refreshAndAccessToken,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -30,12 +32,13 @@ router.route("/refresh-token").post(refreshAndAccessToken);
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
-router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
   .route("/avatar")
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
-
+router.route("/history").get(verifyJWT, getWatchHistory);
 export default router;
